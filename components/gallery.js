b0d1a95e517e19313143d523fb2ed18104614053
gideon-del <gideonchidi471@gmail.com>
2023-09-20 21:02:47 +0100

    loader for search

--- components/gallery.js
+++ components/gallery.js
@@ -4,21 +4,27 @@ import useCanDrag from "@/hooks/useCanDrag";
 import { useFilter } from "@/context/filters";
 import { dummyData } from "@/utils/dummyData";
 const Loader = () => {
-  return <div></div>;
+  return (
+    <div className="absolute top-0 inset-x-0 w-full h-[90vh] grid place-items-center bg-black">
+      <span className="loader"></span>
+    </div>
+  );
 };
 
 const Gallery = () => {
   const Grid = useCanDrag();
-  const { filter, query } = useFilter();
+  const { filter, query, querying } = useFilter();
   const items = query.trim().length > 0 ? filter : dummyData;
   return (
     <>
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 relative">
         <section className="px-14 md:px-20 max-w-5xl mx-auto">
-          <h1>Gallery</h1>
+          <h1 className="capitalize">{query || "Gallery"}</h1>
+
           <Grid items={items} />
         </section>
+        {querying && <Loader />}
       </main>
     </>
   );
--- components/guestGrid.js
+++ components/guestGrid.js
@@ -3,15 +3,15 @@ import { motion } from "framer-motion";
 import GuestItem from "./guestItem";
 import { dummyData } from "@/utils/dummyData";
 import { useFilter } from "@/context/filters";
-const GuestGrid = ({ items }) => {
-  const { filters, query } = useFilter();
-  const images = items;
+const GuestGrid = () => {
+  const { filters } = useFilter();
+
   return (
     <motion.div
       layout
       className=" columns-1 md:columns-2 lg:columns-3 space-y-5 "
     >
-      {images.map((img) => (
+      {filters.map((img) => (
         <GuestItem key={img.id} img={img} />
       ))}
     </motion.div>
